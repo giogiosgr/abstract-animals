07a5654f976dbab60b9a7ac284c5c56a68abbf2f
Giovanni Ricciardi <104310904+giogiosgr@users.noreply.github.com>
2024-06-26 13:37:49 +0200

    feat: istanza e uso delle classi

--- src/main/java/org/abstractanimals/java/Main.java
+++ src/main/java/org/abstractanimals/java/Main.java
@@ -3,8 +3,28 @@ package org.abstractanimals.java;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Istanza e utilizzo delle sottoclassi della classe astratta Animale
+		Cane doge = new Cane();
+		Passerotto birb = new Passerotto();
+		Delfino curioso = new Delfino();
+		Aquila usa = new Aquila();
+		
+	    doge.verso();
+	    doge.mangia();
+	    doge.dormi();
+	    
+	    birb.verso();
+	    birb.mangia();
+	    birb.dormi();
+	    
+	    curioso.verso();
+	    curioso.mangia();
+	    curioso.dormi();
+	    
+	    usa.verso();
+	    usa.mangia();
+	    usa.dormi();
+	    
 	}
 
 }
